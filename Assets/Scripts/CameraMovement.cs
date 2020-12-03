41d8540933d8e8aa58fc9b9fc1a765652aedee66
stacy <stacyy83@gmail.com>
2020-12-02 20:13:04 -0500

    update light & camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float speed = 0.06f;
+    float zoomSpeed = 10.0f;
+    float rotateSpeed = 0.1f;
+
+    float maxHeigth = 13f;
+    float minHeight = 0.6f;
+
+    Vector2 p1;
+    Vector2 p2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hsp = speed * Input.GetAxis("Horizontal");
+        float vsp = speed * Input.GetAxis("Vertical");
+        float scrollSp = -zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 veticalMove = new Vector3(0, scrollSp, 0);
+        Vector3 lateralMove = hsp * transform.right;
+        Vector3 forwardMove = transform.forward;
+        forwardMove.y = 0;
+        forwardMove.Normalize();
+        forwardMove *= vsp;
+
+        Vector3 move = veticalMove + lateralMove + forwardMove;
+
+        transform.position += move;
+
+        getGamerRotation();
+    }
+
+    void getGamerRotation()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            p1 = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            p2 = Input.mousePosition;
+
+            float dx = (p2 - p1).x * rotateSpeed;
+            float dy = (p2 - p1).y * rotateSpeed;
+
+            transform.rotation *= Quaternion.Euler(new Vector3(0, dx, 0));
+            transform.GetChild(0).transform.rotation *= Quaternion.Euler(new Vector3(-dy, 0, 0));
+
+            p1 = p2;
+        }
+    }
+}
